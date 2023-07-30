0e476e26cd7f521f5898aa7b9549fc846fd631a6
IakoGM <jakgonza@student.42urduliz.com>
2023-07-30 22:07:17 +0200

    working with testing.c file

--- testing.c
+++ testing.c
@@ -1,23 +1,21 @@
-#include "push_swap.h"
+// #include "push_swap.h"
 #include "libft/libft.h"
 
 #include <stdio.h>
 // #include <stdlib.h>
 // #include <ctype.h>
 
-int ft_iako(const char *nptr)
+int ft_atoi(const char *nptr)
 {
 	int num;
-	int sign;
 
 	num = 0;
-	sign = 1;
 	while (*nptr >= '0' && *nptr <= '9')
 	{
 		num = num * 10 + *nptr - '0';
 		nptr++;
 	}
-	if (*nptr > '9')
+	if (*nptr <= '0' && *nptr >= '9')
 		return (-1);
 	return (num);
 }
@@ -25,12 +23,11 @@ int ft_iako(const char *nptr)
 int main(int argc, char const *argv[])
 {
 	int i;
-	// int	cast;
 
 	i = 1;
 	while (argc > i)
 	{
-		if (ft_iako(argv[i]) == -1)
+		if (ft_atoi(argv[i]) == -1)
 			printf("El argumento %d, contiene el dato = %s\n", i, argv[i]);
 		else
 			printf("El argumento %d, contiene el numero = %s\n", i, argv[i]);
