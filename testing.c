174baadc3e52b8712afb6467194e481a50af58ca
IakoGM <jakgonza@student.42urduliz.com>
2023-08-09 16:15:16 +0200

    atoi deleted from testing.c

--- testing.c
+++ testing.c
@@ -5,21 +5,6 @@
 // #include <stdlib.h>
 // #include <ctype.h>
 
-int ft_atoi(const char *nptr)
-{
-	int num;
-
-	num = 0;
-	while (*nptr >= '0' && *nptr <= '9')
-	{
-		num = num * 10 + *nptr - '0';
-		nptr++;
-	}
-	if (!(*nptr >= '0' && *nptr <= '9') && *nptr != '\0')
-		return (-1);
-	return (num);
-}
-
 int main(int argc, char const *argv[])
 {
 	int i;
