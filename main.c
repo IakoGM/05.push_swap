1240d41ab2cdd6ab9cf1fb8c35824e3a278687f0
IakoGM <jakgonza@student.42urduliz.com>
2023-08-09 14:08:40 +0200

    fix a single alpha in args

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jakgonza <jakgonza@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 17:01:59 by jakgonza          #+#    #+#             */
-/*   Updated: 2023/08/08 17:20:24 by jakgonza         ###   ########.fr       */
+/*   Updated: 2023/08/09 14:02:09 by jakgonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -113,6 +113,26 @@ int	ft_compare(char **str, char **nums)
 	return (1);
 }
 
+int	ft_check_duplicates(int *numbers, int *cont)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < *cont)
+	{
+		j = 0;
+		while (j < *cont)
+		{
+			if (numbers[i] == numbers[j] && i != j)
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
 void	ft_checkdata(int argc, char const **argv)
 {
 	char	**str;
@@ -129,12 +149,13 @@ void	ft_checkdata(int argc, char const **argv)
 		str = ft_split(argv[1], 32);
 	else
 		str = ft_insert_data(&*argv);
+	// FIXME -> si va una letra sola, tanto en "y" como en argumento y el atoi() devuelve un 0...
 	numbers = ft_fill_numbers(str, cont);
-	// while (i < *cont)
-	// {
-	// 	printf("Numbers int* %d es: %d\n", i, numbers[i]);
-	// 	i++;
-	// }
+	while (i < *cont)
+	{
+		printf("Numbers int* %d es: %d\n", i, numbers[i]);
+		i++;
+	}
 	nums = ft_return_data(numbers, cont);
 	// i = 0;
 	// while (nums[i])
@@ -143,21 +164,75 @@ void	ft_checkdata(int argc, char const **argv)
 	// 	i++;
 	// }
 	if (ft_compare(str, nums))
-		printf("Las cadenas son iguales");
-	else
 	{
-		ft_free(nums);
-		free(numbers);	
-		ft_error(str);
+		printf("Las cadenas son iguales\n");
+		if (!ft_check_duplicates(numbers, cont))
+		{
+			printf("No hay numeros duplicados.\n");
+			return ;
+		}
 	}
+	ft_free(nums);
+	free(numbers);	
+	ft_error(str);	
 }
+
+int	ft_check_empty(char const **argv)
+{
+	int	i;
+
+	i = 0;
+	while (argv[i])
+	{
+		if (argv[i][0] == '\0')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_only_spaces(char const **argv)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (argv[i])
+	{
+		while (argv[i][j] == 32 || argv[i][j] == 9)	
+			j++;
+		if (argv[i][j] == '\0')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+// void	ft_fill_stack(t_stack a, int *numbers)
+// {
+	
+// }
 
 int	main(int argc, char const *argv[])
 {
+	// t_stack	a;
+	// t_stack	b;
+	// int	*numbers;
+
 	if (argc == 1)
 		return (0);
-	// TODO:
-		// - Segmentar los argumentos recibidos entre comillas y los que no
-	ft_checkdata(argc, argv); // Comprobar si argv y &*argv funcionan igual
+	if (ft_check_empty(argv))
+	{
+		write(2, "Error\n", 6);
+		exit(2);
+	}
+	if (ft_only_spaces(argv))
+	{
+		write(2, "Error\n", 6);
+		exit(2);
+	}
+	ft_checkdata(argc, argv);
+	// ft_fill_stack(a, numbers);	
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jakgonza <jakgonza@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 13:52:51 by jakgonza          #+#    #+#             */
-/*   Updated: 2023/08/07 11:49:18 by jakgonza         ###   ########.fr       */
+/*   Updated: 2023/08/09 13:06:11 by jakgonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-typedef struct s_node
+typedef struct s_stack
 {
 	int				index;
 	int				content;
-	struct s_node	*next;
-}					t_node;
+	struct s_stack	*next;
+}					t_stack;
 
 void	ft_free(char **ptr);
 void	ft_error(char **ptr);
